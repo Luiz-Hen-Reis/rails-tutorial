7ef3cb63df7213e22c2db6ed858fe866617e53b4
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-10-14 16:03:35 -0300

    more about models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, :description, presence: true
+    validates :description, length: { minimum: 5 }
+
+    before_save do
+        self.title = self.title + " Luiz"
+    end
 end
